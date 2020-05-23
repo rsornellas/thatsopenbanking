2d1ab921976d0c6158b9eec689b3be0b53b07132
Raphael Ornellas <raphaelso@live.com>
2020-05-23 19:27:49 -0300

    init

---
+++ src/layouts/Default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="layout">
+    <slot/>
+  </div>
+</template>
+
+<style>
+* {
+  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
+}
+
+body {
+  background: #000;
+}
+</style>
